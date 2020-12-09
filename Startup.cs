de6998e62ae6a05874e532c02612da0f8857c189
MelleNout <mellenout@live.nl>
2020-12-09 22:04:59 +0100

    Update Dockerfile

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace Commander
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddEntityFrameworkNpgsql().AddDbContext<CommanderContext>(options =>
+            services.AddDbContext<CommanderContext>(options =>
             {
                 var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
                 string connStr;
